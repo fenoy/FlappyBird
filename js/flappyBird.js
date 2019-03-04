5647237bb280cdf70566d683a4b516f5be6d15f2
fenoy <afenoy95@gmail.com>
2019-03-04 21:36:01 +0100

    small changes

--- js/flappyBird.js
+++ js/flappyBird.js
@@ -5,8 +5,8 @@ let canvas = document.getElementById('window');
 let ctx = canvas.getContext('2d');
 
 // Images
-let bg = new Item();
-let fg = new Item();
+let bg = new Item("/img/bg.png", 0, 0, canvas.width, canvas.height);
+let fg = new Item("/img/fg", 0, 0.75 * canvas.width, 0.75 * canvas.height);
 
 // Draw images
 function draw () {
