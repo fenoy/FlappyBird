070429926425aff64bb5acc5e75bbd6cc75c1514
fenoy <afenoy95@gmail.com>
2019-03-05 08:37:13 +0100

    small changes

--- js/flappyBird.js
+++ js/flappyBird.js
@@ -2,11 +2,14 @@
 let canvas = document.getElementById('window');
 let ctx = canvas.getContext('2d');
 
+// Params
 const params = {
-    'fps':  30,
-    'g': 9.8
+    'fps': 30,
+    't': 10,
+    'g': 10
 };
 
+// Objects
 class Item {
     constructor(imagePath, x, y, width, height, vx=0, vy=0) {
         this.imagePath = imagePath;
@@ -20,13 +23,13 @@ class Item {
     }
 
     updatePosition () {
-        this.x += this.vx * (1 / params.fps);
-        this.y += this.vy * (1 / params.fps);
+        this.x += this.vx * (params.t / params.fps);
+        this.y += this.vy * (params.t / params.fps);
     }
 
-    updateVelocity () {
-        this.vx += ax * (1 / params.fps);
-        this.vy += ay * (1 / params.fps);
+    updateVelocity (ax, ay) {
+        this.vx += ax * (params.t / params.fps);
+        this.vy += ay * (params.t / params.fps);
     }
 
     loadImage () {
@@ -45,27 +48,36 @@ let bg = new Item(
     canvas.width, canvas.height
 );
 
+let bird = new Item(
+    "./img/bird.png",
+    0.2 * canvas.width, 0.3 * canvas.height,
+    60, 50
+);
+
 let fg = new Item(
     "./img/fg.png",
     0, 0.75 * canvas.height,
     canvas.width, 0.25 * canvas.height
 );
 
-let bird = new Item(
-    "./img/bird.png",
-    0.3 * canvas.width, 0.3 * canvas.height,
-    60, 50
-);
-
 // Draw images
 function draw () {
-    bg.loadImage();
-    fg.loadImage();
-    bird.loadImage();
 
+    // Load images
+    bg.loadImage();
+    bird.loadImage();
+    fg.loadImage();
+
+    // Update position and velocity
+    bird.updateVelocity(0, params.g);
+
+    bird.updatePosition();
+
+    // Draw next frame
     setTimeout(() => {
         requestAnimationFrame(draw);
     }, 1000 / params.fps);
 }
 
 document.addEventListener('DOMContentLoaded', draw());
+document.addEventListener('keydown', () => {bird.vy = -40});
