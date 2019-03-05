ae804c15ee9111d1a4c4208d7b10c3cf35875b2a
fenoy <afenoy95@gmail.com>
2019-03-05 16:00:55 +0100

    final version

--- js/flappyBird.js
+++ js/flappyBird.js
@@ -4,10 +4,11 @@ let ctx = canvas.getContext('2d');
 
 // Params
 const params = {
-    'fps': 30,
-    't': 10,
-    'g': 10
+    't': 2,
+    'g': 0.1
 };
+
+let score = 0;
 
 // Objects
 class Item {
@@ -23,13 +24,13 @@ class Item {
     }
 
     updatePosition () {
-        this.x += this.vx * (params.t / params.fps);
-        this.y += this.vy * (params.t / params.fps);
+        this.x += this.vx * params.t;
+        this.y += this.vy * params.t;
     }
 
     updateVelocity (ax, ay) {
-        this.vx += ax * (params.t / params.fps);
-        this.vy += ay * (params.t / params.fps);
+        this.vx += ax * params.t;
+        this.vy += ay * params.t;
     }
 
     loadImage () {
@@ -54,30 +55,139 @@ let bird = new Item(
     60, 50
 );
 
-let fg = new Item(
-    "./img/fg.png",
-    0, 0.75 * canvas.height,
-    canvas.width, 0.25 * canvas.height
-);
+let fg = [
+    new Item(
+        "./img/fg.png",
+        0, 0.75 * canvas.height,
+        canvas.width, 0.25 * canvas.height,
+        -params.t, 0
+    )
+];
+
+let gapY = 0.1 * canvas.height + ((0.75 - 0.1 - 0.2) * canvas.height * Math.random());
+let pipe = [
+    {
+        'top': new Item("./img/pipeTop.png",
+            canvas.width, gapY - canvas.height,
+            0.1 * canvas.width, canvas.height,
+            -params.t, 0
+        ),
+        'bot': new Item("./img/pipeBot.png",
+            canvas.width, gapY + 0.2 * canvas.height,
+            0.1 * canvas.width, canvas.height,
+            -params.t, 0
+        )
+    }
+];
 
 // Draw images
-function draw () {
+async function draw () {
 
     // Load images
     bg.loadImage();
     bird.loadImage();
-    fg.loadImage();
+    pipe.forEach((item) => {
+        item.top.loadImage();
+        item.bot.loadImage()
+    });
+    fg.forEach((item) => {item.loadImage()});
+
+    // Print score
+    ctx.fillStyle = "#000000";
+    ctx.font = "30px Arial";
+    ctx.fillText("Score : " + score, 20, canvas.height-20);
+
+
+    // Add and remove items from queues
+    if (fg[fg.length - 1].x === 0) {
+        fg.push(
+            new Item(
+                "./img/fg.png",
+                canvas.width, 0.75 * canvas.height,
+                canvas.width, 0.25 * canvas.height,
+                -params.t, 0
+            )
+        )
+    }
+
+    if (fg[0].x === -canvas.width) {
+        fg.shift()
+    }
+
+    if (pipe[pipe.length - 1].top.x === 0.5 * canvas.width) {
+        gapY = 0.1 * canvas.height + ((0.75 - 0.1 - 0.2) * canvas.height * Math.random());
+        pipe.push(
+            {
+                'top': new Item("./img/pipeTop.png",
+                    canvas.width, gapY - canvas.height,
+                    0.1 * canvas.width, canvas.height,
+                    -params.t, 0
+                ),
+                'bot': new Item("./img/pipeBot.png",
+                    canvas.width, gapY + 0.2 * canvas.height,
+                    0.1 * canvas.width, canvas.height,
+                    -params.t, 0
+                )
+            }
+        );
+    }
+
+    if (pipe[0].top.x === -0.2 * canvas.width)  {
+        pipe.shift()
+    }
 
     // Update position and velocity
     bird.updateVelocity(0, params.g);
 
     bird.updatePosition();
+    pipe.forEach((item) => {
+        item.top.updatePosition();
+        item.bot.updatePosition()
+    });
+    fg.forEach((item) => {item.updatePosition()});
+
+    // Update score
+    if (pipe[0].top.x + pipe[0].top.width === bird.x + bird.width) {
+        score++
+    }
 
     // Draw next frame
-    setTimeout(() => {
-        requestAnimationFrame(draw);
-    }, 1000 / params.fps);
+    if (
+        (bird.x + bird.width - 10 < pipe[0].top.x || bird.x + 10 > pipe[0].top.x + pipe[0].top.width)
+        || (bird.y + 10 > pipe[0].top.y + pipe[0].top.height && bird.y + bird.height - 10 < pipe[0].bot.y)
+
+    ) {requestAnimationFrame(draw)} else {
+        await sleep(500);
+        endScreen()
+    }
 }
 
-document.addEventListener('DOMContentLoaded', draw());
-document.addEventListener('keydown', () => {bird.vy = -40});
+function endScreen() {
+    // Load text
+    ctx.textAlign = "center";
+
+    ctx.fillStyle = "#ff0000";
+    ctx.font = "70px Arial";
+    ctx.fillText("GAME OVER ", 0.5 * canvas.width, 0.4 * canvas.height);
+
+    ctx.fillStyle = "#000000";
+    ctx.font = "30px Arial";
+    ctx.fillText("Press any key to play again ", 0.5 * canvas.width, 0.5 * canvas.height);
+
+    ctx.textAlign = "left";
+    ctx.fillStyle = "#000000";
+    ctx.font = "30px Arial";
+    ctx.fillText("Score : " + score, 20, canvas.height-20);
+
+    // Reload option
+    document.addEventListener('keydown', () => {
+        location.reload()
+    })
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+document.addEventListener('DOMContentLoaded', draw);
+document.addEventListener('keydown', () => {bird.vy = -40 * params.g});
